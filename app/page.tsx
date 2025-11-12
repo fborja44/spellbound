8f78d7c14f0a10b9cbfc17b45db22a773ca91825
Francis Borja <49008508+fborja44@users.noreply.github.com>
2025-11-11 23:23:51 -0500

    feat: game layout changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,12 @@ import { Timer } from 'lucide-react';
 export default function Home() {
 	return (
 		<main className='flex flex-row items-stretch justify-center gap-4 p-24 w-full'>
-			<div className='container-col gap-3 w-20'>
-				<div className='flex flex-row items-end gap-1 game-header-h text-2xl font-extrabold'>
-					<Timer className='size-7 stroke-3' />
-					<span>3/4</span>
+			<div className='container-col gap-3 min-w-20'>
+				<div className='flex flex-row items-end game-header-h text-2xl font-extrabold'>
+					<div className='container-row gap-1'>
+						<Timer className='size-6 stroke-3 text-slate-400' />
+						<span>3/4</span>
+					</div>
 				</div>
 				<EnergyBar amount={0} />
 				<RefreshButton />
@@ -20,6 +22,14 @@ export default function Home() {
 			<div className='container-col gap-2.5'>
 				<WordDisplay />
 				<Board />
+			</div>
+			<div className='flex flex-col gap-1 w-fit min-w-20'>
+				<div className='flex flex-col justify-center game-header-h'>
+					<span className='uppercase font-bold text-sm text-slate-400'>
+						Score
+					</span>
+					<span className='uppercase font-bold text-2xl leading-5'>0</span>
+				</div>
 			</div>
 		</main>
 	);
