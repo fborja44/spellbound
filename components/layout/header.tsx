4cc5aae79a12129f6d5c430b13f49fea2ae990f9
Francis Borja <49008508+fborja44@users.noreply.github.com>
2025-12-09 17:19:12 -0500

    minor: bump version

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,14 +1,18 @@
 import { Sparkles } from 'lucide-react';
 import NewGameButton from '../buttons/new-game-button';
 import Link from 'next/link';
+import { Badge } from '../ui/badge';
 
 const Header = () => {
 	return (
 		<header className='container-row justify-between h-16 px-4 w-full'>
 			<Link href='/'>
 				<h1 className='container-row gap-2 font-bold text-xl hover:text-yellow-200 transtion-colors duration-200'>
-					<Sparkles strokeWidth={2.5} />
+					<Sparkles strokeWidth={2.5} fill='white' />
 					<span>spellbound</span>
+					<small className='bg-slate-800 text-slate-300 px-2 py-0.5 rounded-full text-xs font-bold'>
+						alpha v0.5.0
+					</small>
 				</h1>
 			</Link>
 			<div className='container-row gap-2'>
